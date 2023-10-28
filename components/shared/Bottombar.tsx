b2dcbba3cddff0db6c9cf1e6799a739242db2caf
amh6281 <amh6281@naver.com>
2023-10-28 09:46:04 +0900

    폴더 구조 변경 2023.10.28

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { sidebarLinks } from "@/constants";
+import { sidebarLinks } from "../../constants/index.js";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { sidebarLinks } from "@/constants";
+import { sidebarLinks } from "../../constants/index.js";
 import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
