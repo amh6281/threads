8c187e29802e191dbee403aa3323b775d455fb65
amh6281 <amh6281@naver.com>
2023-11-01 20:08:19 +0900

    user action 생성 2023.11.01
    server action 기능을 통해 user 데이터 처리

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
+): Promise<void> {
+  connectToDB();
+
+  try {
+    await User.findOneAndUpdate(
+      {
+        id: userId,
+      },
+      { username: username.toLowerCase(), name, bio, image, onboarded: true },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      // 경로 재 유효화
+      revalidatePath(path);
+    }
+  } catch (err: any) {
+    throw new Error(`Failed to create/update user: ${err.message}`);
+  }
+}
