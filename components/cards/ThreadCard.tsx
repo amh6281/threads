a1e54ab579b4a544ffe7a7faf01e7c02649da308
amh6281 <amh6281@naver.com>
2023-11-07 20:18:34 +0900

    SingleThread page comment rendering 2023.11.07

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -33,7 +33,11 @@ const ThreadCard = ({
   isComment,
 }: Props) => {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
@@ -91,6 +95,14 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtitle-medium text-gray-1">
+                    {comments.length}
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
